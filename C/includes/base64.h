7d52379ad6cf54919dc76de628ea839f81d01119
banahaker <pn0818x@outlook.com>
2024-02-13 21:39:52 +0800

    feat: defined decoder function type

--- C/includes/base64.h
+++ C/includes/base64.h
@@ -3,5 +3,7 @@
 
 char *base64_encoder(const char *data, size_t len);
 
+char *base64_decoder(const char *data, size_t len);
+
 static const char *encoding =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
