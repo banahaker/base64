eaa19bee7216a7af46e0720916cd6dcdf303edf8
banahaker <pn0818x@outlook.com>
2024-02-13 20:54:43 +0800

    fix: padding logic of encoder

--- C/base64.c
+++ C/base64.c
@@ -2,35 +2,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *base64_encoder(char *in, size_t len_in);
+char *base64_encoder(const char *data, size_t len);
 
 static const char *encoding =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 int main() {
-  printf("hello, Base64!\n");
-  printf("%s\n", base64_encoder("Hello, world", 12));
+  printf("Hello, world!\n");
+  printf("%s\n", base64_encoder("Hello, world!", 13));
   return 0;
 }
 
-char *base64_encoder(char *in, size_t len_in) {
-  size_t len_out = 4 * (len_in + 2) / 3;
-  char *out = malloc(len_out + 1);
+char *base64_encoder(const char *data, size_t len) {
+  size_t out_len = (len + 2) / 3 * 4;
+  char *out = malloc(out_len + 1);
   size_t i, j;
 
-  for (i = 0, j = 0; i < len_in; i += 3) {
-    uint32_t n = (in[i] << 16) | (in[i + 1] << 8) | in[i + 2];
+  for (i = 0, j = 0; i < len; i += 3) {
+    uint32_t n = 0;
+    int padding = 0;
+
+    n |= ((unsigned char)data[i]) << 16;
+    padding++;
+
+    if (i + 1 < len) {
+      n |= ((unsigned char)data[i + 1]) << 8;
+      padding++;
+    }
+
+    if (i + 2 < len) {
+      n |= ((unsigned char)data[i + 2]);
+      padding++;
+    }
+
     out[j++] = encoding[(n >> 18) & 0x3F];
     out[j++] = encoding[(n >> 12) & 0x3F];
-    out[j++] = encoding[(n >> 6) & 0x3F];
-    out[j++] = encoding[n & 0x3F];
+
+    if (padding > 1)
+      out[j++] = encoding[(n >> 6) & 0x3F];
+    else
+      out[j++] = '=';
+
+    if (padding > 2)
+      out[j++] = encoding[n & 0x3F];
+    else
+      out[j++] = '=';
   }
 
-  while (j < len_out) {
-    out[j++] = '=';
-  }
-
-  out[len_out] = '\0';
-
+  out[out_len] = '\0';
   return out;
 }
